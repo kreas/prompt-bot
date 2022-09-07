df46feaafdfda42651c8cd4bf2bd0c6628a71cf3
Timothy Warren <tim.warren@huntgathercreate.co>
2022-09-07 01:59:16 -0500

    Adds og tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,12 @@ const Home: NextPage = () => {
         <title>Scrollrack</title>
         <meta name="description" content="AI powered asset generator for Magic The Gathering" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+        <meta property="og:title" content="AI powered asset generator for Magic the Gathering" />
+        <meta property="og:description" content="Create unique tokens, playmats, and more," />
+        <meta
+          property="og:image"
+          content="https://www.scrollrack.quest/og-image.jpg"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
