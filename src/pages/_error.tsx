b0e54a904dffb12ee6b7b6bd32289e09caebc86c
Timothy Warren <tim.warren@huntgathercreate.co>
2022-09-20 18:14:59 -0500

    updates _error.tsx

---
+++ src/pages/_error.tsx
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/nextjs'
+import NextErrorComponent from 'next/error'
+
+const CustomErrorComponent = (props: any) => {
+  return <NextErrorComponent statusCode={props.statusCode} />
+}
+
+CustomErrorComponent.getInitialProps = async (contextData: any) => {
+  await Sentry.captureUnderscoreErrorException(contextData)
+  return NextErrorComponent.getInitialProps(contextData)
+}
+
+export default CustomErrorComponent
