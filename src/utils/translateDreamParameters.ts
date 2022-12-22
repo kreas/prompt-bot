7ef5e8a356c94ea12af8cda7993fd1c54217fe67
Tim Warren <kreas@pm.me>
2022-12-22 10:42:39 -0600

    changes the default number of steps

--- src/utils/translateDreamParameters.ts
+++ src/utils/translateDreamParameters.ts
@@ -1,15 +1,14 @@
 const size = (n: number) => {
   const m = 64 * n
   const r = m % 64
+
   return r === 0 ? m : m + 64 - r
 }
 
 export const aspectToPixels = (aspectRatio: string) => {
-  // 64 * 12 = 768
-
   switch (aspectRatio) {
     case '1:1':
-      return { width: size(8), height: size(8) }
+      return { width: size(9), height: size(9) }
     case '2:3':
       return { width: size(8), height: size(12) }
     case '3:2':
@@ -17,21 +16,21 @@ export const aspectToPixels = (aspectRatio: string) => {
     case '16:9':
       return { width: size(12), height: size(6) }
     default:
-      return { width: size(8), height: size(8) }
+      return { width: size(9), height: size(9) }
   }
 }
 
 export const qualityToSteps = (quality: string) => {
   switch (quality) {
     case 'low':
-      return 25
+      return 20
     case 'mid':
-      return 50
+      return 25
     case 'high':
-      return 75
+      return 40
     case 'max':
-      return 100
-    default:
       return 50
+    default:
+      return 25
   }
 }
